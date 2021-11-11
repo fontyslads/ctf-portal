6950252a566ad0a7c945f94a4355090db879a27d
SamJammoulQS <93433314+SamJammoulQS@users.noreply.github.com>
2021-11-11 10:13:00 +0100

    reCAPTCHA

--- ctf-portal-frontend/src/App.tsx
+++ ctf-portal-frontend/src/App.tsx
@@ -2,13 +2,29 @@ import React from "react";
 import "./App.scss";
 
 //components
-import FlagList from "./components/FlagList/FlagList";
+
+import Flag from "./models/Flag";
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
+import {Button} from "react-bootstrap";
+
+
+function GoogleRecaptchaExample() {
+  return null;
+}
 
 class App extends React.Component {
+
   render() {
     return (
       <div className="App">
-        <FlagList />
+        {/*<FlagList />*/}
+
+        <GoogleReCaptchaProvider
+            reCaptchaKey="6LdeqJkcAAAAAFcW4LbVNriRt-fMTu0DZHBrYb-0"
+            language="en-AU"
+         >
+        <Button>testttttttfg</Button>
+        </GoogleReCaptchaProvider>
       </div>
     );
   }
